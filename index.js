f2b14b20a184a36b5a58a3d3f5601831ba18cbf1
EC2 Default User <ec2-user@ip-172-31-15-234.us-west-2.compute.internal>
2018-04-25 09:39:18 +0000

    test

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var express = require("express");
 var app = express();
 const oktaClient = require("./lib/oktaClient");
 
+app.get("/test", (req, res) => {
+  res.send("hello worldddd!!!!");
+});
+
 /* Create a new User (register). */
 app.post("/api/add-okta-user", (req, res, next) => {
-  if (!req.body) return res.sendStatus(400);
-  
+  console.log(req.body);
+
   const newUser = {
     profile: {
       firstName: req.body.firstName,
@@ -19,7 +23,7 @@ app.post("/api/add-okta-user", (req, res, next) => {
       }
     }
   };
-  
+
   oktaClient
     .createUser(newUser)
     .then(user => {
@@ -27,14 +31,25 @@ app.post("/api/add-okta-user", (req, res, next) => {
       res.send(user);
     })
     .catch(err => {
-      res.status(400);
-      res.send(err);
+      console.log(err);
+      // res.status(400);
+      // res.send(err);
     });
-    
 });
 
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  ); // If needed
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,contenttype"); // If needed
+  res.setHeader("Access-Control-Allow-Credentials", true); // If needed
 
+  // Pass to next layer of middleware
+  next();
+});
 
 app.listen(8000, function() {
-  console.log("Example app listening on port 8000!");
+  console.log("serving on port 8000");
 });
